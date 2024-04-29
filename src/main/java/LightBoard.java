d12bc0a9243f9b73ea806b072ab7c8fde87d988e
finleycaul <143015925+finleycaul@users.noreply.github.com>
2024-04-29 10:08:27 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,7 +10,7 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    lights = new boolean(numRows, numCols);
+    lights = new boolean [numRows] [numCols];
     for (int r = 0; r<numRows; r++){
       for (int c = 0; c<numCols; c++){
         if(Math.random()<0.4){
