3149e7b85109ec3b7ce0e720136a859f9a74c453
finleycaul <143015925+finleycaul@users.noreply.github.com>
2024-04-29 10:11:13 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -34,9 +34,9 @@ public class LightBoard
         numOn++;
       }
     }
-    if(lights[row][col]==true && numOn%2==0){
+    if(lights[row][col] && numOn%2==0){
       return false;
-    } if(lights[row][col]==false && numOn%3 ==0){
+    } if(!lights[row][col] && numOn%3 ==0){
       return true;
     }
     return lights [row][col];
