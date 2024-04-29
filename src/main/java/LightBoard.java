72d70b025067a7c1dd87c6fe5253b330ea3fd7aa
finleycaul <143015925+finleycaul@users.noreply.github.com>
2024-04-29 10:05:58 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,8 +10,16 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
-
+    lights = new boolean(numRows, numCols);
+    for (int r = 0; r<numRows; r++){
+      for (int c = 0; c<numCols; c++){
+        if(Math.random()<0.4){
+          lights[r][c]=true;
+        }else{
+          lights[r][c]= false;
+        }
+      }
+    }
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
@@ -20,7 +28,19 @@ public class LightBoard
    */
   public boolean evaluateLight(int row, int col)
   {
-    /* to be implemented in part (b) */
+    numOn = 0;
+    for(int r = 0; r<lights.length; r++){
+      if(lights[r][col]){
+        numOn++;
+      }
+    }
+    if(lights[row][col]==true && numOn%2==0){
+      return false;
+    } if(lights[row][col]==false && numOn%3 ==0){
+      return true;
+    }
+    return lights [row][col];
+  }
    
  
   }
